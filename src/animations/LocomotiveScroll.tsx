bb3c1523fb215c91ec21bb4e87c2e8f6b6468b1c
DevAnupShourya <contact.yourwebdev@gmail.com>
2024-05-01 14:11:04 +0530

    locomotive import fix

--- src/animations/LocomotiveScroll.tsx
+++ src/animations/LocomotiveScroll.tsx
@@ -1,18 +1,21 @@
 "use client";
-
 import { useEffect } from 'react';
 import LocomotiveScroll from 'locomotive-scroll';
 
 export default function LocomotiveScrollComponent() {
     useEffect(() => {
-        // ? maybe can cause error on production
-        const scroll = new LocomotiveScroll({ smooth: true });
+        let scroll: LocomotiveScroll;
+        import("locomotive-scroll").then((locomotiveModule) => {
+            scroll = new locomotiveModule.default({
+                smooth: true,
+                smartphone: { smooth: false },
+                resetNativeScroll: true
+            });
+        });
 
         return () => {
-            if (scroll) {
-                scroll.destroy();
-            }
-        };
+            if (scroll) scroll.destroy();
+        }
     }, []);
 
     return <></>;
